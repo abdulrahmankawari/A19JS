be9fa2f06c4fb72e19932eaabb3aac0af79b5720
Abdul Rahmane <dreamchaser4uk2@gmail.com>
2025-05-27 08:26:44 +0530

    feat: sting ,array concat with obj loop

--- demo25.js
+++ demo25.js
@@ -1,9 +1,60 @@
+const Fname="abdul"
+const lName="kawari"
 
-const arr= [1, 2, 3, 4, 5];
-
-const filterEven =()=>{
-   const result= arr.filter((num)=> num % 2 === 0)
-   return result
+const addName=()=>{
+   return `${Fname} rehman ${lName}`
 }
 
-console.log(filterEven());
+// console.log(addName())
+
+const arr1=[-3,-2,-1]
+const arr2=[0,1,2,3]
+
+const addArray=()=>{
+   const result=[...arr1,...arr2]
+   return result
+}
+const addArrayV2=()=>[...arr1,...arr2]
+// console.log(addArray())/
+
+const userData={
+   name:"tripal",
+   email:"tripal123@gmail.com",
+   phn:123,
+   hasPassedOut:false,
+   occupation:undefined
+}
+
+const apiResponse={
+   status:404,
+   message:"not found",
+   data:{}
+}
+
+let thisIsFinalUSerData={}
+
+const populateData=()=>{
+   if(apiResponse.status ===200 && apiResponse.data){
+      thisIsFinalUSerData=apiResponse.data
+   }else{
+      thisIsFinalUSerData=userData
+   }
+   return thisIsFinalUSerData
+}
+console.log(populateData())
+
+
+// const filterFalsyVal=()=>{
+//    let finalUSerData={}
+//    for(let key in userData){
+
+//       if(!userData[key]){
+//          finalUSerData={...finalUSerData,[key]:userData[key]}
+//       }
+         
+//    }
+//    return finalUSerData
+
+// }
+
+// console.log(filterFalsyVal())
