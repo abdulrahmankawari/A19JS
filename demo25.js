19c641195336321888fcd48ebd06653ec6f1f0bd
Abdul Rahmane <dreamchaser4uk2@gmail.com>
2025-06-01 08:49:58 +0530

    feat: bested arry and obj loop

--- demo25.js
+++ demo25.js
@@ -1,60 +1,52 @@
-const Fname="abdul"
-const lName="kawari"
+const customer = [
+   {id:1,name:"aurum",bought:[{id:1,name:"t-shirt"},{id:"2",name:"earphone"}]},
+   {id: 3, name: "sneha", bought: [{id: 5, name: "watch"}, {id: 6, name: "handbag"}]},
+   {id: 4, name: "rohit", bought: [{id: 7, name: "laptop"}, {id: 8, name: "mouse"}]},
+   {id: 5, name: "anaya", bought: [{id: 9, name: "book"}, {id: 10, name: "pen"}]},
+   {id: 6, name: "kabir", bought: [{id: 11, name: "keyboard"}, {id: 12, name: "monitor"}]},
+   {id: 7, name: "diya", bought: [{id: 13, name: "phone-case"}, {id: 14, name: "charger"}]}
 
-const addName=()=>{
-   return `${Fname} rehman ${lName}`
-}
 
-// console.log(addName())
-
-const arr1=[-3,-2,-1]
-const arr2=[0,1,2,3]
-
-const addArray=()=>{
-   const result=[...arr1,...arr2]
-   return result
-}
-const addArrayV2=()=>[...arr1,...arr2]
-// console.log(addArray())/
-
-const userData={
-   name:"tripal",
-   email:"tripal123@gmail.com",
-   phn:123,
-   hasPassedOut:false,
-   occupation:undefined
-}
-
-const apiResponse={
-   status:404,
-   message:"not found",
-   data:{}
-}
-
-let thisIsFinalUSerData={}
-
-const populateData=()=>{
-   if(apiResponse.status ===200 && apiResponse.data){
-      thisIsFinalUSerData=apiResponse.data
-   }else{
-      thisIsFinalUSerData=userData
+] 
+const customerApi = {
+   status: 200,
+   message:"customer data fetched successfully",
+   data: customer,
+};
+const getprodutsName = ()=> {
+   if(customerApi.status === 200) {
+      let boughtProducts = []
+      customerApi.data.forEach((customer)=>{
+         customer.bought.map((item)=>{
+            boughtProducts.unshift(item.name)
+         })
+      })
+      return boughtProducts;
    }
-   return thisIsFinalUSerData
+};
+
+console.log(getprodutsName());
+
+
+const getprodutsName2 = ()=> {
+   if(customerApi.status === 200) {
+      let boughtProducts = {};
+      customerApi.data.forEach((customer)=>{
+         customer.bought.map((item)=>{
+            if(productNumber[item.id]){
+               productNumber[item.id] += 1;
+               
+            }
+            else{
+               productNumber[item.id] = 1;
+            }
+         })
+      })
+      return productNumber;
+   
+
+      
+        
+   }
 }
-console.log(populateData())
-
-
-// const filterFalsyVal=()=>{
-//    let finalUSerData={}
-//    for(let key in userData){
-
-//       if(!userData[key]){
-//          finalUSerData={...finalUSerData,[key]:userData[key]}
-//       }
-         
-//    }
-//    return finalUSerData
-
-// }
-
-// console.log(filterFalsyVal())
+console.log(getprodutsName2());
